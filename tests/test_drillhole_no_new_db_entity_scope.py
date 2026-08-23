dd042180b79138810abb35bdd9fda94a5ac1dba1
Eugene <zhenyaemshanov0@gmail.com>
2026-08-23 21:58:29 +1100

    Remove redundant drillhole contract test

--- tests/test_drillhole_no_new_db_entity_scope.py
+++
@@ -1,7 +0,0 @@
-from database.base import Base
-from database import drillhole_models  # noqa: F401
-
-
-def test_feature_adds_only_revisioned_event_drillhole_dataset_table():
-    drillhole_tables = [name for name in Base.metadata.tables if "drillhole" in name]
-    assert drillhole_tables == ["blast_event_drillhole_datasets"]
